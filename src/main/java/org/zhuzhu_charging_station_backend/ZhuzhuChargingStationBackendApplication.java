d9980a2f7d4555be23ffa45b43023d491bdec67b
Lucia <616524407@qq.com>
2025-05-24 09:26:56 +0800

    项目初始化

---
+++ src/main/java/org/zhuzhu_charging_station_backend/ZhuzhuChargingStationBackendApplication.java
@@ -0,0 +1,13 @@
+package org.zhuzhu_charging_station_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhuzhuChargingStationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhuzhuChargingStationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zhuzhu_charging_station_backend/ZhuzhuChargingStationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zhuzhu_charging_station_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhuzhuChargingStationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
